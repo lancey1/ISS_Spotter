b4944025b600adf9cc18f0b0e78ee9ffb15e07b2
lancey1 <102191108+lancey1@users.noreply.github.com>
2022-05-15 21:09:47 -0400

    Add files via upload

---
+++ issPromise.js
@@ -0,0 +1,29 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json')
+};
+
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const latitude = JSON.parse(body).latitude;
+  const longitude = JSON.parse(body).longitude;
+  return request(`http://api.open-notify.org/iss-now.json?lat=${latitude}&lon=${longitude}`)
+}
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
